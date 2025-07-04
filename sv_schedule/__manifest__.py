fa60089af8c862eef78ecfdbb5656efeefa0eace
GustVillalobos <gustvillalobos92@gmail.com>
2025-07-04 15:01:22 -0600

    Update dependency

--- sv_schedule/__manifest__.py
+++ sv_schedule/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '16.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr_payroll_community','hr_attendance'],
+    'depends': ['base','hr_payroll','hr_attendance'],
 
     # always loaded
     'data': [
